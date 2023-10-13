0dd988ae925621bc57d7e0b59ea43811cf7b4af0
vorozhkog <vorozkhog@icloud.com>
2023-10-13 14:10:07 +0100

    summary regenerated

--- src/settings.py
+++ src/settings.py
@@ -21,7 +21,7 @@ HIDE_DATASET = True  # set False when 100% sure about repo quality
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.GNU_GPL_v2()
-APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.General()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.OCR(is_used=False)]
 CATEGORY: Category = Category.General()
 
 CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
